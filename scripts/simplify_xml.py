fe8e2fdd1475d567d062955213faa5551b1506b9
Algirdas Jarockis <algirdas.jarockis@gmail.com>
2022-04-20 21:27:14 +0300

    Added production data files

--- scripts/simplify_xml.py
+++ scripts/simplify_xml.py
@@ -1,5 +1,7 @@
 import xml.etree.ElementTree as et
 import argparse
+import glob
+import os.path
 
 parser = argparse.ArgumentParser(description='Picks only relevant xml info from game files and writes into new xml file')
 parser.add_argument(dest='dataType', default='fillTypes')
@@ -7,6 +9,9 @@ args = parser.parse_args()
 
 outputFile = 'result.xml'
 
+#
+# Simplifies fillTypes xml
+#
 def processFillTypes(filename):
     tree = et.parse(filename)
     root = tree.getroot()
@@ -35,6 +40,21 @@ def processFillTypes(filename):
 
     print("Content written to %s" % outputFile)
 
+#
+# Simplifies production xml
+#
+def processProductions(filename, outputFilename):
+    tree = et.parse(filename)
+    root = tree.getroot()
+
+    newTree = et.ElementTree(root.find('./productionPoint/productions'))
+    newTree.write(outputFilename, short_empty_elements=True, xml_declaration=True, encoding="utf-8")
+
+    print("Content written to %s" % outputFilename)
+
 if args.dataType == 'fillTypes':
     processFillTypes('../data/fillTypes.xml')
-
+elif args.dataType == 'productions':
+    for sourceFile in glob.glob('../data/productions/source/*.xml'):
+        outputFilePath = '../data/productions/' + os.path.basename(sourceFile)
+        processProductions(sourceFile, outputFilePath)
